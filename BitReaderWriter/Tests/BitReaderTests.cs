a095b40cec07ebe34a8a37836a1a45dd048836e9
Adrian Bunea <adrian_bunea28@yahoo.com>
2019-10-20 18:49:21 +0300

    Write tests to describe requirements

---
+++ BitReaderWriter/Tests/BitReaderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BitReaderTests
+    {
+        [TestMethod]
+        public void ReadBit_ValidFile_ReadsBitCorrectly()
+        {
+
+        }
+        
+        [TestMethod]
+        public void ReadNBit_ValidFile_ReadsNBitsCorrectly()
+        {
+
+        }
+    }
+}
---
+++ BitReaderWriter/Tests/BitWriterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BitWriterTests
+    {
+        [TestMethod]
+        public void WriteBit_ValidFile_WritesBitCorrectly()
+        {
+        }
+
+        [TestMethod]
+        public void WriteNBit_ValidFile_WritesNBitsCorrectly()
+        {
+
+        }
+    }
+}
