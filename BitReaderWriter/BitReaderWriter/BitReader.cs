7367a7e6dd2fdf4403687472bd4a6b5d474cd639
Adrian Bunea <adrian_bunea28@yahoo.com>
2019-10-21 22:35:20 +0300

    Pass first BitReader test

---
+++ BitReaderWriter/BitReaderWriter/BitReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitReaderWriter
+{
+    public class BitReader
+    {
+        private const int LEFTMOST_BIT = 128;
+        private const int RIGHTMOST_BIT = 1;
+
+        private readonly string filepath;
+        private byte[] fileBytes;
+
+        public BitReader(string filepath)
+        {
+            fileBytes = File.ReadAllBytes(filepath);
+        }
+
+        public byte ReadBit()
+        {
+            byte bit = (byte)(fileBytes[0] & RIGHTMOST_BIT);
+            return bit;
+        }
+    }
+}
--- BitReaderWriter/Tests/BitReaderTests.cs
+++ BitReaderWriter/Tests/BitReaderTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using BitReaderWriter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
@@ -6,10 +8,29 @@ namespace Tests
     [TestClass]
     public class BitReaderTests
     {
-        [TestMethod]
-        public void ReadBit_ValidFile_ReadsBitCorrectly()
-        {
+        BitReader bitReader;
+        string filepath = $"{Environment.CurrentDirectory}\\ReaderTestFile.txt";
 
+        [TestCleanup]
+        public void Teardown()
+        {
+            File.Delete(filepath);
+        }
+
+        [TestMethod]
+        [DataRow((byte)61, (byte)1)]
+        [DataRow((byte)62, (byte)0)]
+        [DataRow((byte)63, (byte)1)]
+        [DataRow((byte)64, (byte)0)]
+        public void ReadBit_ValidFile_ReadsBitCorrectly(byte testByte, byte expectedBit)
+        {
+            byte[] testBytes = new byte[1] { testByte };
+            File.WriteAllBytes(filepath, testBytes);
+
+            bitReader = new BitReader(filepath);
+            byte readBit = bitReader.ReadBit();
+
+            Assert.AreEqual(expectedBit, readBit);
         }
         
         [TestMethod]
