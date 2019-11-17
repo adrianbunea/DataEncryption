89ce07a08864d918c03f1ff31d058be559462534
Adrian Bunea <adrian_bunea28@yahoo.com>
2019-11-17 19:54:56 +0200

    Write steps for encoding

--- StaticHuffman/StaticHuffman/StaticHuffman.cs
+++ StaticHuffman/StaticHuffman/StaticHuffman.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,28 @@ namespace StaticHuffman
     class StaticHuffman
     {
         public List<string> Codes { get; set; }
+        private Dictionary<byte, uint> codes;
 
         public void Encode(string fileToBeEncoded)
         {
+            int[] statistic = CalculateStatistic(fileToBeEncoded);
+            codes = CreateModel(statistic);
+            WriteEncodedFile(fileToBeEncoded);
+        }
 
+        private int[] CalculateStatistic(string fileToBeEncoded)
+        {
+            return new int[256];
+        }
+
+        private Dictionary<byte, uint> CreateModel(int[] statistic)
+        {
+            return new Dictionary<byte, uint>();
+        }
+
+        private void WriteEncodedFile(string fileToBeEncoded)
+        {
+            
         }
 
         public void Decode(string fileToBeDecoded)
