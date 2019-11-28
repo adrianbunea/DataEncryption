cead2a12390d3132eb8dcf860a8028022d3f58e5
Adrian Bunea <adrian_bunea28@yahoo.com>
2019-11-29 00:27:10 +0200

    Write button functions

--- LZW/LZW/Form1.cs
+++ LZW/LZW/Form1.cs
@@ -12,6 +12,11 @@ namespace LZW
 {
     public partial class Form1 : Form
     {
+        readonly string root = $"{Environment.CurrentDirectory}";
+        string fileToBeEncoded;
+        string fileToBeDecoded;
+        LZW lzw;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,22 +24,57 @@ namespace LZW
 
         private void loadToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileToBeEncoded = openFileDialog.FileName;
+                }
+            }
         }
 
         private void encodeToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            lzw = new LZW();
+            if (radioButtonEmpty.Checked)
+            {
+                lzw.Strategy = 0;
+            } 
+            else if (radioButtonFreeze.Checked)
+            {
+                lzw.Strategy = 1;
+            }
+            else
+            {
+                throw new Exception("Unknown strategy. Can be either Freeze or Empty.");
+            }
 
+            lzw.Encode(fileToBeEncoded);
+            if (checkBoxShowIndexes.Checked)
+            {
+                listBoxIndexes.DataSource = lzw.Indexes;
+            }
         }
 
         private void loadToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileToBeDecoded = openFileDialog.FileName;
+                }
+            }
         }
 
         private void decodeToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            lzw = new LZW();
+            lzw.Decode(fileToBeDecoded);
+            if (checkBoxShowIndexes.Checked)
+            {
+                listBoxIndexes.DataSource = lzw.Indexes;
+            }
         }
     }
 }
---
+++ LZW/LZW/LZW.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LZW
+{
+    class LZW
+    {
+        public List<string> Indexes { get; }
+        private List<int> indexes;
+
+        public int Strategy
+        {
+            set
+            {
+                strategy = value;
+            }
+        }
+        private int strategy;
+
+        public void Encode(string filepath)
+        {
+
+        }
+
+        public void Decode(string filepath)
+        {
+
+        }
+    }
+}
